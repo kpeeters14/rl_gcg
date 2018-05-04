2dec2b6a60d1ca987d9c1d146de37d5cbfea6da2
Kevin Peeters <kevinpeeters04@gmail.com>
2018-05-04 13:56:28 +0200

    Cleaned up some files

--- ROS/scripts/postscript.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-RemoteHost=$(cat $_CONDOR_JOB_AD | grep RemoteHost | head -1 | cut -d '=' -f 2 | cut -d '@' -f 2 | cut -d '.' -f 1)
-
-rm /tmp/singlebel
-
-# singularity fails to kill all consequently.
-killall -u r0453462
--- ROS/scripts/start_singularity.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# echo "exec $1 in singularity image /gluster/visics/singularity/ros_gazebo_tensorflow.imgs"
-# cd /gluster/visics/singularity
-# pwd
-# ls /gluster/visics/singularity
-# sleep 1
-# /usr/bin/singularity exec --nv /gluster/visics/singularity/ros_gazebo_tensorflow.imgs /home/kevin/catkin_ws/src/rl_gcg/mainscript.sh
-singularity exec --nv /home/kevin/ros_gazebo_tensorflow.img /home/kevin/catkin_ws/src/rl_gcg/scripts/mainscript.sh
