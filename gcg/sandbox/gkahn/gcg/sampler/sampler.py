6dc8f3f8036e802f059bf048c2e617ef46e83c09
Kevin Peeters <kevinpeeters04@gmail.com>
2018-05-08 10:32:15 +0200

    Made the algorithm wait while everything is resetting

--- gcg/sandbox/gkahn/gcg/sampler/sampler.py
+++ gcg/sandbox/gkahn/gcg/sampler/sampler.py
@@ -120,9 +120,8 @@ class RNNCriticSampler(object):
 
         ### take step
         # next_observations, rewards, dones, env_infos = self._vec_env.step(actions)
-        self._interface_gcg.pub_gcg_ready()
         while (self._interface_gcg.get_ready_for_action() == False):
-            pass
+            self._interface_gcg.pub_gcg_ready()
         next_observations, rewards, dones, env_infos = self._interface_gcg.take_step(actions)
 
         if (rewards[0] == -1):
